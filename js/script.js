5ff46a35bb76d7b12f2ae4d8ccaed9c35391f8e6
Ashot0 <illia.holovan@gmail.com>
2023-06-27 09:22:31 +0300

    cute_not_work

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
-// Строгий режим
-"use strict";
 
+const inWork = document.querySelector('.in-work')
+const sumbol = document.querySelector('.symbol')
+const inProcess = document.querySelector('.in-process')
+
+const calculate = false;
+
+function appendValue(valu) {
+	if (calculate == false) {
+		inProcess.value = toString(valu);
+		calculate = true;
+	} else {
+		inWork.text = toString(valu);
+		calculate = false;
+	}
+
+}
